91f750bc9decc1af2b2e1ee6d2eadb2f6547673c
Muhammad Ramshad <muhammadramshad07@email.com>
2025-09-15 17:26:52 +0530

    User register and login

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
-
+const userRouter = require('./routes/user.routes')
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+const connectToDB = require('./config/db');
+connectToDB();
+const cookieParser = require("cookie-parser");
+
 
 app.set('view engine', 'ejs');
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 
 
-
-app.get('/', (req, res) =>{
-    res.render('index');
-})
+app.use('/user', userRouter);
 
 
 app.listen(3000, ()=>{
